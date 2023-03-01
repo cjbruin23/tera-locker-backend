77f9dd803201587c9a88d50a7575cdb29c221d15
Colin Bruin <colinjb23@gmail.com>
2023-03-01 10:05:27 -0800

    implemented auth into app, currently getting unauthorized without providing the access token, as expected

--- dist/index.js
+++ dist/index.js
@@ -4,13 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const express_oauth2_jwt_bearer_1 = require("express-oauth2-jwt-bearer");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const app = (0, express_1.default)();
 const port = process.env["PORT"];
+const app = (0, express_1.default)();
+// Right now, protecting all routes
+app.use((0, express_oauth2_jwt_bearer_1.auth)({
+    issuerBaseURL: `${process.env["AUTH0_DOMAIN"]}`,
+    audience: `${process.env["AUTH0_AUDIENCE"]}`,
+}));
 app.get("/", (req, res) => {
     console.log("req", req);
     res.send("Express + TypeScript Server");
+});
+app.get("/files", (req, res) => {
+    console.log("get files", req);
+    res.send("You do have access to this resource");
 });
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
 import express, { Request, Response } from "express";
+import { auth } from "express-oauth2-jwt-bearer";
 import dotenv from "dotenv";
 
 dotenv.config();
+const port = process.env["PORT"];
 
 const app = express();
-const port = process.env["PORT"];
+
+// Right now, protecting all routes
+app.use(
+  auth({
+    issuerBaseURL: `${process.env["AUTH0_DOMAIN"]}`,
+    audience: `${process.env["AUTH0_AUDIENCE"]}`,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   console.log("req", req);
   res.send("Express + TypeScript Server");
 });
 
-app.get("files/", (req: Request, res: Response) => {
+app.get("/files", (req: Request, res: Response) => {
   console.log("get files", req);
   res.send("You do have access to this resource");
 });
