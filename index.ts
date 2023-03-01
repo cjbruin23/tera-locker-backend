f0dcf3e2c9ee157f45b5da5773c4dadba82048c6
Colin Bruin <colinjb23@gmail.com>
2023-03-01 08:04:45 -0800

    adding env to gitignore"

--- index.ts
+++ index.ts
@@ -11,6 +11,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
+app.get("files/", (req: Request, res: Response) => {
+  console.log("get files", req);
+  res.send("You do have access to this resource");
+});
+
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
